89214e9956169dd5fc86f20666ca7dfd3a1ea5ec
lenadgh <102828285+lenadgh@users.noreply.github.com>
2022-04-21 09:36:57 -0700

    Create certstatus.ps1

---
+++ certstatus.ps1
@@ -0,0 +1,46 @@
+$excelfile = "c:\temp\emaillist.xlsx"
+$sheetname = "sheet2"
+function get-URLlist{
+	$Excel = New-Object -ComObject Excel.Application
+	## open the spreadsheet
+    $source = $excelfile
+	$Workbook = $Excel.Workbooks.Open($source,2,$true)
+	## select the worksheet in excel file
+	$ws = $Workbook.Sheets.Item("$sheetname")
+	## create new PSO for email list
+	$URLList = @()
+	## Calculate number of used range
+	$UsedRange = $ws.UsedRange
+    ## specify the column
+    $URLLIST += $UsedRange.Cells.Item(1,8).EntireColumn.value2 | Select-Object -Skip 1
+    return $URLLIST
+    ## close workbook
+    $workbook.close($false)
+    $Excel.Quit()
+}
+
+$URLlist = get-URLlist
+$HTTPSsites = @()
+
+foreach ($URL in $URLList){
+    if($URL.Contains('https://')){
+        $HTTPSsites += $URL}
+}
+$certstatus = @()
+
+foreach ($HTTPSsite in $HTTPSsites) {
+    [Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+    $req = $null
+    $req = [Net.HttpWebRequest]::Create($httpssite) 
+    $req.Timeout = '3600'
+    $req.AllowAutoRedirect = $false
+    $req.GetResponse() | Out-Null
+    [datetime]$expiration = [System.DateTime]::Parse($req.ServicePoint.Certificate.GetExpirationDateString())
+    $output = [PSCustomObject]@{
+        'site' = $httpssite
+        'Cert End Date' = $expiration
+        'Expires in' = ($expiration - $(get-date)).Days
+        }
+    $certstatus += $output
+}
+$certstatus
